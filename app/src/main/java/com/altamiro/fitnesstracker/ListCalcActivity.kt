8abbe6bb5b2eabf2bf2382dcaecb2e5e7cce5b8c
brunobarross <altamiro_bruno@yahoo.com.br>
2024-05-30 07:46:42 -0300

    exibindo dados do banco

--- app/src/main/java/com/altamiro/fitnesstracker/ListCalcActivity.kt
+++ app/src/main/java/com/altamiro/fitnesstracker/ListCalcActivity.kt
@@ -5,15 +5,19 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.altamiro.fitnesstracker.model.Calc
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 class ListCalcActivity : AppCompatActivity() {
     private lateinit var rv_list_calc: RecyclerView
@@ -21,59 +25,56 @@ class ListCalcActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_list_calc)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        val result = mutableListOf<Calc>()
+        val adapter = ListCalcAdapter(result)
+        rv_list_calc = findViewById(R.id.rv_list_calc)
+        rv_list_calc.layoutManager = LinearLayoutManager(this)
+        rv_list_calc.adapter = adapter
+
         val type = intent?.extras?.getString("type") ?: throw IllegalArgumentException("type not found")
+
+
 
         Thread{
             val app = application as App
             val dao = app.db.calcDao()
             val response = dao.getRegisterByType(type)
-            runOnUiThread{
-                Log.i("Teste", "resposta: $response")
-
-                val listAdapter = this@ListCalcActivity.ListAdapter(response)
-                rv_list_calc.adapter = listAdapter
-                rv_list_calc.layoutManager = LinearLayoutManager(this)
-
-
-            }
+            result.addAll(response)
+            // avisa que foi inserido novos dados e renderiza novamente
+            adapter.notifyDataSetChanged()
 
         }.start()
     }
-    private inner class ListAdapter(private val listData: List<Calc>) :
-        RecyclerView.Adapter<ListAdapter.ViewHolder>() {
 
-        // Subclasse interna ViewHolder dentro de MainAdapter
-        inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    private inner class ListCalcAdapter(
+        private val listCalc: List<Calc>,
+    ) :
+        RecyclerView.Adapter<ListCalcAdapter.ListViewHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
+            val view = layoutInflater.inflate(android.R.layout.simple_list_item_1, parent, false)
+            return ListViewHolder(view)
+        }
 
-            // Método para configurar os componentes de visualização com os dados do item atual
-            fun bind(item: Calc){
-                val name: TextView = itemView.findViewById(android.R.id.text1)
-                name.setText(item.res.toString())
+        override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
+            val itemCurrent = listCalc[position]
+            holder.bind(itemCurrent)
+        }
+
+        override fun getItemCount(): Int {
+            return listCalc.size
+        }
+        private inner class ListViewHolder(iTemView: View) : RecyclerView.ViewHolder(iTemView) {
+            fun bind(item: Calc) {
+                val textV = itemView as TextView
+                val sdf = SimpleDateFormat("dd/MM/yyyy HH:mm", Locale("pt", "br"))
+                val data = sdf.format(item.createdAt)
+                val res = item.res
+                textV.text = getString(R.string.list_response, res, data)
 
             }
         }
 
-        // Sobrescreve o método para criar e retornar um novo ViewHolder
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-            val view = LayoutInflater.from(parent.context).inflate(android.R.layout.simple_list_item_1, parent, false)
-            return ViewHolder(view)
-        }
-
-        // Sobrescreve o método para retornar o número total de itens na lista
-        override fun getItemCount(): Int {
-            return listData.size
-        }
-
-        // Sobrescreve o método para vincular os dados do item atual ao ViewHolder correspondente
-        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-            val itemCurrent = listData[position]
-            holder.bind(itemCurrent)
-        }
     }
+
 
 }
